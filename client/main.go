e422ed2b9b39623839529f6288e120d95f1b25c8
Julien Perrenoud <julien.perrenoud@jfperren.io>
2018-09-28 18:32:41 +0200

    Add params for client

--- client/main.go
+++ client/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+  "fmt"
+  "flag"
+)
+
 
 func main () {
-  fmt.Printf("test")
+
+  // Define Flags
+
+  uiPort := flag.String("UIPort", "8080", "port for the UI client")
+  message := flag.String("msg", "127.0.0.1:5000", "message to be sent")
+
+  flag.Parse()
+
+  // Print Flags
+
+  fmt.Printf("port = %v\n", *uiPort)
+  fmt.Printf("message = %v\n", *message)
 }
--- scripts/build.sh
+++
@@ -1,9 +0,0 @@
-
-# Build client and gossiper
-go build
-cd client
-go build
-cd ..
-
-# Rename Peerster into gossiper
-mv ./Peerster ./gossiper
