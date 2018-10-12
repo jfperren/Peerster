792b4fab1074805a90613c20fb15eb65d3f5fbae
Julien Perrenoud <julien.perrenoud@jfperren.io>
2018-10-13 00:53:57 +0200

    More testing

--- utils_test.go
+++ utils_test.go
@@ -60,3 +60,18 @@ func TestInsertSortedMiddle(t *testing.T) {
 		t.Errorf("Expected array to be %v, got %v instead.", []uint{0, 1, 2}, array)
 	}
 }
+
+// Tests that a Gossiper correctly creates a Status packet based on its rumors.
+func TestInsertSortedThird(t *testing.T) {
+
+	array := []uint32{0, 1}
+	array = insertSorted(array, 2)
+
+	if len(array) != 3 {
+		t.Errorf("Array size should be %v, got %v instead.", 2, len(array))
+	}
+
+	if !(array[0] == 0 && array[1] == 1 && array[2] == 2) {
+		t.Errorf("Expected array to be %v, got %v instead.", []uint{0, 1, 2}, array)
+	}
+}
