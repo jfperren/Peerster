95f2b4a49577ff8d9e01dab1277fff8f3c3001a6
Julien Perrenoud <julien.perrenoud@jfperren.io>
2018-11-08 15:32:40 +0100

    Add DataReply and DataRequest types of messages and some helpers

--- common/messages.go
+++ common/messages.go
@@ -1,43 +1,65 @@
 package common
 
+import (
+    "bytes"
+    "crypto/sha256"
+)
+
 // --
 // -- DATA STRUCTURES
 // --
 
 type SimpleMessage struct {
-    OriginalName  string
-    RelayPeerAddr string
-    Contents      string
+    OriginalName    string
+    RelayPeerAddr   string
+    Contents        string
 }
 
 type RumorMessage struct {
-    Origin        string
-    ID            uint32
-    Text          string
+    Origin          string
+    ID              uint32
+    Text            string
+}
+
+type DataRequest struct {
+    Origin          string
+    Destination     string
+    HopLimit        uint32
+    HashValue       []byte
+}
+
+type DataReply struct {
+    Origin          string
+    Destination     string
+    HopLimit        uint32
+    HashValue       []byte
+    Data            []byte
 }
 
 type PrivateMessage struct {
-    Origin        string
-    ID            uint32
-    Text          string
-    Destination   string
-    HopLimit      uint32
+    Origin          string
+    ID              uint32
+    Text            string
+    Destination     string
+    HopLimit        uint32
 }
 
 type PeerStatus struct {
-    Identifier    string
-    NextID        uint32
+    Identifier      string
+    NextID          uint32
 }
 
 type StatusPacket struct {
-    Want          []PeerStatus
+    Want            []PeerStatus
 }
 
 type GossipPacket struct {
-    Simple        *SimpleMessage
-    Rumor         *RumorMessage
-    Status        *StatusPacket
-    Private       *PrivateMessage
+    Simple          *SimpleMessage
+    Rumor           *RumorMessage
+    Status          *StatusPacket
+    Private         *PrivateMessage
+    DataRequest     *DataRequest
+    DataReply       *DataReply
 }
 
 // --
@@ -67,7 +89,7 @@ func (simple *SimpleMessage) Packed() *GossipPacket {
         panic("Cannot pack <nil> Simple into a GossipPacket")
     }
 
-    return &GossipPacket{simple,nil,nil, nil}
+    return &GossipPacket{simple,nil,nil, nil, nil, nil}
 }
 
 // Pack a RumorMessage into a GossipPacket
@@ -77,7 +99,7 @@ func (rumor *RumorMessage) Packed() *GossipPacket {
         panic("Cannot pack <nil> rumor into a GossipPacket")
     }
 
-    return &GossipPacket{nil,rumor,nil, nil}
+    return &GossipPacket{nil,rumor,nil, nil, nil, nil}
 }
 
 // Pack a StatusPacket into a GossipPacket
@@ -87,23 +109,43 @@ func (status *StatusPacket) Packed() *GossipPacket {
         panic("Cannot pack <nil> status into a GossipPacket")
     }
 
-    return &GossipPacket{nil,nil,status, nil}
+    return &GossipPacket{nil,nil,status, nil, nil, nil}
 }
 
-// Pack a PrivateMessage into a GossipPacket
-func (private *PrivateMessage) Packed() *GossipPacket {
+// Pack a DataRequest into a GossipPacket
+func (request *DataRequest) Packed() *GossipPacket {
 
-    if private == nil {
-        panic("Cannot pack <nil> status into a GossipPacket")
+    if request == nil {
+        panic("Cannot pack <nil> data request into a GossipPacket")
     }
 
-    return &GossipPacket{nil,nil,nil, private}
+    return &GossipPacket{nil,nil,nil, nil, request, nil}
 }
 
-// Checks if a given GossipPacket is valid. It is only valid if exactly one of its 4 fields is non-nil.
+// Pack a DataReply into a GossipPacket
+func (reply *DataReply) Packed() *GossipPacket {
+
+    if reply == nil {
+        panic("Cannot pack <nil> data reply into a GossipPacket")
+    }
+
+    return &GossipPacket{nil,nil,nil, nil, nil, reply}
+}
+
+
+// Checks if a given GossipPacket is valid. It is only valid if exactly one of its 6 fields is non-nil.
 func (packet *GossipPacket) IsValid() bool {
     return boolCount(packet.Rumor != nil) + boolCount(packet.Simple != nil) +
-        boolCount(packet.Status != nil) + boolCount(packet.Private != nil) == 1
+        boolCount(packet.Status != nil) + boolCount(packet.Private != nil) +
+        boolCount(packet.DataReply != nil) + boolCount(packet.DataRequest != nil) == 1
+}
+
+func (reply *DataReply) VerifyHash(expected []byte) bool {
+
+    dataIsConsistent := bytes.Compare(sha256.Sum256(reply.Data)[:], reply.HashValue) == 0
+    hashIsExpected := bytes.Compare(reply.HashValue, expected) == 0
+
+    return dataIsConsistent && hashIsExpected
 }
 
 func boolCount(b bool) int {
