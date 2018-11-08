383aaf42c0988b350a0c78d183659bce1461fbfc
Julien Perrenoud <julien.perrenoud@jfperren.io>
2018-11-08 15:33:21 +0100

    First iteration of a file system for the gossiper

--- gossiper/files.go
+++ gossiper/files.go
@@ -1,27 +1,200 @@
 package gossiper
 
 import (
+	"bytes"
 	"crypto/sha256"
+	"encoding/hex"
 	"fmt"
 	"github.com/jfperren/Peerster/common"
 	"os"
-	"strconv"
 )
 
-type File struct {
+type MetaFile struct {
 	name  string
 	size  int
 	hash  []byte
+	data  []byte
+}
+
+type Chunk struct {
+	hash  []byte
+	data  []byte
+}
+
+func NewMetaFile(name string, data []byte) *MetaFile {
+	hash := sha256.Sum256(data)
+	return &MetaFile{name, 0, hash[:] , data}
+}
+
+func NewChunk(data []byte) *Chunk {
+	hash := sha256.Sum256(data)
+	return &Chunk{hash[:], data}
+}
+
+type FileSystem struct {
+	chunks map[string]*Chunk
+	metaFiles map[string]*MetaFile
+}
+
+func (fs FileSystem) getMetaFile(hash []byte) (*MetaFile, bool) {
+	v, found := fs.metaFiles[hex.EncodeToString(hash)]
+	return v, found
+}
+
+func (fs FileSystem) getChunk(hash []byte) (*Chunk, bool) {
+	v, found := fs.chunks[hex.EncodeToString(hash)]
+	return v, found
+}
+
+func (metaFile *MetaFile) countOfChunks() int {
+	return len(metaFile.data) / sha256.Size
+}
+
+func (metaFile *MetaFile) hashAt(index int) []byte {
+	return metaFile.data[index * common.FileChunkSize: (index + 1) * common.FileChunkSize]
+}
+
+func (chunk *Chunk) size() int {
+	return len(chunk.data)
+}
+
+func (chunk *Chunk) isLastIn(metaFile *MetaFile) bool {
+	lastHash := metaFile.hashAt(metaFile.countOfChunks())
+	return bytes.Compare(lastHash, chunk.hash) == 0
+}
+
+func (fs FileSystem) storeMetaFile(metaFile *MetaFile) bool {
+
+	key := hex.EncodeToString(metaFile.hash)
+
+	_, found := fs.metaFiles[key]
+
+	if found {
+		return false
+	}
+
+	fs.metaFiles[key] = metaFile
+
+	go metaFile.save()
+
+	return true
+}
+
+func (fs FileSystem) storeChunk(chunk *Chunk) bool {
+
+	key := hex.EncodeToString(chunk.hash)
+
+	metaFile, found := fs.metaFiles[key]
+
+	if found {
+		return false
+	}
+
+	metaFile.size += chunk.size()
+	fs.chunks[key] = chunk
+
+	go chunk.save()
+
+	if chunk.isLastIn(metaFile) {
+		fs.reconstructFile(metaFile.hash)
+	}
+
+	return true
+}
+
+
+func (fs FileSystem) saveDataReply(name string, reply *common.DataReply) bool {
+
+	metaFile, found := fs.getMetaFile(reply.HashValue)
+
+	if !found {
+
+		metaFile = NewMetaFile(name, reply.Data)
+		return fs.storeMetaFile(metaFile)
+
+	} else {
+
+		chunk := NewChunk(reply.Data)
+		return fs.storeChunk(chunk)
+	}
+}
+
+func (fs FileSystem) reconstructFile(metaHash []byte) {
+
+	data := make([]byte, 0)
+
+	metaFile, found := fs.getMetaFile(metaHash)
+
+	if !found {
+		panic("File not found")
+	}
+
+	for i := 0; i < metaFile.countOfChunks(); i++ {
+
+		hash := metaFile.hashAt(i)
+		chunk, found := fs.getChunk(hash)
+
+		if !found {
+			panic("Chunk not found")
+		}
+
+		data = append(data, chunk.data...)
+	}
+
+	filePath := common.SharedFilesDir + metaFile.name
+	file, err := os.OpenFile(filePath, os.O_WRONLY|os.O_CREATE, 0644)
+	if err != nil { fmt.Print(err) }
+	file.Write(data)
+
+}
+
+func (fs FileSystem) downloadStatus(metaHash []byte) ([]byte, bool) {
+
+	metaFile, found := fs.getMetaFile(metaHash)
+
+	if !found {
+		return metaHash, false
+	}
+
+	for i := 0; i < metaFile.countOfChunks(); i++ {
+
+		hash := metaFile.hashAt(i)
+
+		_, found := fs.getChunk(hash)
+
+		if !found {
+			return hash, false
+		}
+	}
+
+	return make([]byte, 0), true
+}
+
+func (chunk *Chunk) save() {
+
+	chunkPath := common.SharedFilesDir + hex.EncodeToString(chunk.hash) + common.ChunkFileSuffix
+	chunkFile, err := os.OpenFile(chunkPath, os.O_WRONLY|os.O_CREATE, 0644)
+	if err != nil { fmt.Print(err) }
+	chunkFile.Write(chunk.data)
+
+}
+
+func (meta *MetaFile) save() {
+
+	metaPath := common.SharedFilesDir + meta.name + common.MetaFileSuffix
+	metaFile, err := os.OpenFile(metaPath, os.O_WRONLY|os.O_CREATE, 0644)
+	if err != nil { fmt.Print(err) }
+	metaFile.Write(meta.data)
+
 }
 
 // Prepares a file so as to make it available to send on the network. Chunk + computes hash
-func ScanFile(fileName string) *File {
+func (fs FileSystem) ScanFile(fileName string) {
 
 	// Open file for reading
 	filePath := common.SharedFilesDir + fileName
 	file, err := os.Open(filePath)
 	if err != nil { fmt.Print(err) }
-
 
 	buff := make([]byte, common.FileChunkSize)
 	size := 0
@@ -40,10 +213,8 @@ func ScanFile(fileName string) *File {
 		size += count
 
 		// Write chunk into separate file
-		chunkPath := common.SharedFilesDir + fileName + common.ChunkFileSuffix + strconv.Itoa(i)
-		chunkFile, err := os.OpenFile(chunkPath, os.O_WRONLY|os.O_CREATE, 0644)
-		if err != nil { fmt.Print(err) }
-		chunkFile.Write(buff)
+		chunk := &Chunk{hash[:], buff}
+		chunk.save()
 
 		common.DebugScanChunk(i, hash[:])
 
@@ -56,14 +227,9 @@ func ScanFile(fileName string) *File {
 	metaHash := sha256.Sum256(hashes)
 
 	// Create and open meta file
-	metaPath := common.SharedFilesDir + fileName + common.MetaFileSuffix
-	metaFile, err := os.OpenFile(metaPath, os.O_WRONLY|os.O_CREATE, 0644)
-	if err != nil { fmt.Print(err) }
+	metaFile := &MetaFile{fileName, size,metaHash[:],hashes}
+	metaFile.save()
 
 	common.DebugScanFile(fileName, size, metaHash[:])
 
-	// Write list of hashes in meta file
-	metaFile.Write(hashes)
-
-	return &File{fileName,size,metaHash[:]}
 }
