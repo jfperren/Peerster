43a24d876717e215859db2745e4c7681ab4b7413
Julien Perrenoud <julien.perrenoud@jfperren.io>
2018-11-09 14:23:15 +0100

    Update client, add functions to handle files & use udpsocket

--- client/main.go
+++ client/main.go
@@ -1,12 +1,11 @@
 package main
 
 import (
+  "encoding/hex"
   "flag"
   "fmt"
   "github.com/dedis/protobuf"
   "github.com/jfperren/Peerster/common"
-  "github.com/jfperren/Peerster/gossiper"
-  "net"
 )
 
 
@@ -17,8 +16,8 @@ func main () {
   uiPort := flag.String("UIPort", "8080", "port for the UI client")
   message := flag.String("msg", "Test message", "message to be sent")
   dest := flag.String("dest", "", "destination for the private message")
-  file := flag.String("file", "", "file to be indexed by the gossiper")
-
+  file := flag.String("file", "", "file to be indexed by the gossiper, or filename of the requested file")
+  request := flag.String("request", "", "request a chunk or metafile of this hash")
 
   flag.Parse()
 
@@ -28,40 +27,60 @@ func main () {
   fmt.Printf("message = %v\n", *message)
   fmt.Printf("dest = %v\n", *dest)
   fmt.Printf("file = %v\n", *file)
+  fmt.Printf("request = %v\n", *request)
 
-  fmt.Printf("Sending message '%v' to port %v\n", *message, *uiPort)
+  // Packet to send
+  var packet *common.GossipPacket
 
-  // Simply sends message via UDP. Note that we need to use a port different
-  // from UIPort otherwise there will be some errors.
+  switch {
 
-  if *file != "" {
-      gossiper.ScanFile(*file)
+  case *request != "":
+
+    if *file == "" {
+      panic("Cannot request a file without giving a name")
+    }
+
+    if *dest == "" {
+      panic("Cannot request a file without specifying the destination node")
+    }
+
+    hash, err := hex.DecodeString(*request)
+
+    if err != nil {
+      panic("Error decoding hash specified in 'request'")
+    }
+
+    request := &common.DataRequest{*file, *dest, common.InitialHopLimit, hash}
+    packet = request.Packed()
+
+  case *file != "":
+
+    reply := &common.DataReply{"client", *file, 0, make([]byte, 0), make([]byte, 0)}
+    packet = reply.Packed()
+
+  case *dest != "":
+
+    private := &common.PrivateMessage{"client", 0, *message, *dest, common.InitialHopLimit}
+    packet = private.Packed()
+
+  case *message != "":
+
+    simple := &common.SimpleMessage{"client", "", *message}
+    packet = simple.Packed()
+
+  default:
+
+    panic("No message specified, unclear instructions.")
   }
-
-  // Resolves local address
-  localAddr, err := net.ResolveUDPAddr("udp4", ":5050")
-  if err != nil { panic(err) }
-
-  // Bind local address
-  conn, err := net.ListenUDP("udp4", localAddr)
-  if err != nil { panic(err) }
-
-  // Then resolves address for gossiper
-  uiAddr, err := net.ResolveUDPAddr("udp4", ":" + *uiPort)
-  if err != nil { panic(err) }
-
-  // Creates packet
-  packet := (&common.SimpleMessage{"", "", *message}).Packed()
 
   // Encodes message
   bytes, err := protobuf.Encode(packet)
   if err != nil { panic(err) }
 
-  // Sends message bytes to gossiper via UDP
-  _,  err = conn.WriteToUDP(bytes, uiAddr)
-  if err != nil { panic(err) }
+  // Send
+  socket := common.NewUDPSocket(":5050")
+  socket.Send(bytes, ":" + *uiPort)
+  socket.Unbind()
 
-  // Close connection
-
-  // conn.Close()
 }
+
