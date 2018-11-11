75b6fa8037feca2bd50234fe1e6f2e38acec4d89
Julien Perrenoud <julien.perrenoud@jfperren.io>
2018-11-11 18:32:00 +0100

    Write server code for routes /uploadFile and /requestFile

--- gossiper/webserver.go
+++ gossiper/webserver.go
@@ -1,9 +1,11 @@
 package gossiper
 
 import (
+	"encoding/hex"
 	"encoding/json"
 	"github.com/jfperren/Peerster/common"
 	"net/http"
+	"strconv"
 )
 
 var g *Gossiper
@@ -23,6 +25,17 @@ type Message struct {
 	Text string
 }
 
+type File struct {
+	Name string
+	Hash string
+}
+
+type FileRequest struct {
+	Name string
+	Destination string
+	Hash string
+}
+
 func StartWebServer(gossiper *Gossiper, port string) {
 
 	// Stores gossiper
@@ -37,6 +50,8 @@ func StartWebServer(gossiper *Gossiper, port string) {
 	http.HandleFunc("/node", middleware(handleNode))
 	http.HandleFunc("/user", middleware(handleUser))
 	http.HandleFunc("/privateMessage", middleware(handlePrivateMessage))
+	http.HandleFunc("/fileRequest", middleware(handleFileRequest))
+	http.HandleFunc("/fileUpload", middleware(handleFileUpload))
 
 	go func () {
 		err := http.ListenAndServe(":" + port, nil)
@@ -199,6 +214,132 @@ func handlePrivateMessage(res http.ResponseWriter, req *http.Request) {
 
 		privateMessage := common.NewPrivateMessage("", message.Destination, message.Text)
 		g.HandleClient(privateMessage.Packed())
+
+		res.WriteHeader(http.StatusOK)
+
+	case "GET":
+
+		indexHeader := req.Header.Get("x-index")
+		index, err := strconv.Atoi(indexHeader)
+
+		if err != nil {
+			res.WriteHeader(http.StatusBadRequest)
+			json.NewEncoder(res).Encode("Error decoding 'x-index' parameter")
+			return
+		}
+
+		if index < 0 {
+			res.WriteHeader(http.StatusBadRequest)
+			json.NewEncoder(res).Encode("Index should be bigger or equal to 0.")
+		}
+
+		var body []*common.PrivateMessage
+
+		if  index >= len(g.Messages) {
+			body = make([]*common.PrivateMessage, 0)
+		} else {
+			body = g.Messages[index:]
+		}
+
+		res.WriteHeader(http.StatusOK)
+		json.NewEncoder(res).Encode(body)
+
+	default:
+		res.WriteHeader(http.StatusMethodNotAllowed)
+	}
+}
+
+func handleFileUpload(res http.ResponseWriter, req *http.Request) {
+
+	switch req.Method {
+
+	case "POST":
+
+		var filename string
+		err := json.NewDecoder(req.Body).Decode(&filename)
+
+		if err != nil {
+			res.WriteHeader(http.StatusBadRequest)
+			json.NewEncoder(res).Encode(err)
+			return
+		}
+
+		if filename == "" {
+			res.WriteHeader(http.StatusBadRequest)
+			json.NewEncoder(res).Encode("Filename cannot be blank")
+			return
+		}
+
+		metaFile, err := g.FileSystem.ScanFile(filename)
+
+		if err != nil {
+			res.WriteHeader(http.StatusBadRequest)
+			json.NewEncoder(res).Encode(err.Error())
+			return
+		}
+
+		res.WriteHeader(http.StatusOK)
+		json.NewEncoder(res).Encode(File{
+			metaFile.Name,
+			hex.EncodeToString(metaFile.Hash),
+		});
+
+	case "GET":
+
+		metaFiles := make([]File, 0)
+
+		for _, metaFile := range(g.FileSystem.metaFiles) {
+			metaFiles = append(metaFiles, File{
+				metaFile.Name,
+				hex.EncodeToString(metaFile.Hash),
+			})
+		}
+
+		res.WriteHeader(http.StatusOK)
+		json.NewEncoder(res).Encode(metaFiles)
+
+	default:
+		res.WriteHeader(http.StatusMethodNotAllowed)
+	}
+}
+
+func handleFileRequest(res http.ResponseWriter, req *http.Request) {
+
+	switch req.Method {
+
+	case "POST":
+
+		var request FileRequest
+		err := json.NewDecoder(req.Body).Decode(&request)
+
+		if err != nil {
+			res.WriteHeader(http.StatusBadRequest)
+			json.NewEncoder(res).Encode(err)
+			return
+		}
+
+		if request.Name == "" || request.Hash == "" || request.Destination == "" {
+			res.WriteHeader(http.StatusBadRequest)
+			json.NewEncoder(res).Encode("Fields cannot be blank")
+			return
+		}
+
+		hash, err := hex.DecodeString(request.Hash)
+
+		if err != nil {
+			res.WriteHeader(http.StatusBadRequest)
+			json.NewEncoder(res).Encode(err)
+			return
+		}
+
+		dataRequest := &common.DataRequest{
+			request.Name,
+			request.Destination,
+			common.InitialHopLimit,
+			hash,
+		}
+
+		g.HandleClient(dataRequest.Packed())
 
 		res.WriteHeader(http.StatusOK)
 
