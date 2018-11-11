83973e035f784653093538c7139f73e98b319328
Julien Perrenoud <julien.perrenoud@jfperren.io>
2018-11-11 19:12:58 +0100

    Add previously ignored scripts to run & build

---
+++ scripts/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Build client and gossiper
+go build
+cd client
+go build
+cd ..
+
+# Rename Peerster into gossiper
+# mv ./Peerster ./gossiper
---
+++ scripts/run_bob_server.sh
@@ -0,0 +1,1 @@
+./Peerster -gossipAddr=127.0.0.1:5001 -UIPort=8081 -name="Bob" -peers=127.0.0.1:5000 -server -rtimer=5 -separatefs
