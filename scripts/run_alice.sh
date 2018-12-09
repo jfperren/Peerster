9bddd92362d4756b361d03b70770f606adf74d9b
Julien Perrenoud <julien.perrenoud@jfperren.io>
2018-12-09 20:17:20 +0100

    Remove -separatefs and -verbose flag on default run scripts to match specs

--- scripts/run_alice.sh
+++ scripts/run_alice.sh
@@ -1,1 +1,1 @@
-./Peerster -UIPort=8080 -gossipAddr=127.0.0.1:5000 -name=Alice -peers=127.0.0.1:5002 -rtimer=5  -separatefs
+./Peerster -UIPort=8080 -gossipAddr=127.0.0.1:5000 -name=Alice -peers=127.0.0.1:5002 -rtimer=5
