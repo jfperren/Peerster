fe017c928decceba9c2dd2db58324cfbc791b6eb
Julien Perrenoud <julien.perrenoud@jfperren.io>
2018-12-09 18:50:34 +0100

    Add script for submission

---
+++ scripts/submit.sh
@@ -0,0 +1,25 @@
+
+rm -rf submission/*
+
+# mkdir ../tmp
+cp -rf . ../tmp
+cd ../tmp
+rm -rf submission
+rm Peerster
+rm -rf _SharedFiles/*
+rm -rf _Downloads/*
+rm -rf logs/*
+cd ../Peerster
+
+name="Peerster-jfperren-v3.0.0"
+
+mkdir submission/github.com
+mkdir submission/github.com/jfperren
+
+mv ../tmp submission/github.com/jfperren
+cd submission/github.com/jfperren/
+mv tmp Peerster
+cd ../..
+
+tar -zcvf $name.tar.gz github.com
+rm -rf github.com
