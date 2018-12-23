a919b4d15f37d948d4f393f63c1e7f51a989c2e0
Moi <guillaume.vizier@polytechnique.edu>
2018-12-23 18:05:12 +0100

    First steps to integrate crypto in the Meexter

---
+++ gossiper/crypto.go
@@ -0,0 +1,63 @@
+package gossiper
+
+import (
+    "crypto"
+    "crypto/rand"
+    "crypto/rsa"
+    "crypto/sha256"
+    "fmt"
+)
+
+type Crypto struct {
+    PrivateKey *rsa.PrivateKey
+}
+
+func (c *Crypto) GenerateKey() {
+    privateKey, err := rsa.GenerateKey(rand.Reader, 4096)
+    if err != nil {
+        panic(err)
+    }
+    c.PrivateKey = privateKey
+}
+
+func (c *Crypto) PublicKey() crypto.PublicKey {
+    return c.PrivateKey.Public()
+}
+
+func (c *Crypto) Decypher(payload []byte) []byte {
+    decyphered, err := rsa.DecryptOAEP(sha256.New(), rand.Reader, c.PrivateKey, payload, []byte{})
+    if err != nil {
+        fmt.Println("Error from decryption: %s\n", err)
+        return []byte{}
+    }
+    return decyphered
+}
+
+func (c *Crypto) Cypher(payload []byte, publicKey rsa.PublicKey) []byte {
+    cyphered, err := rsa.EncryptOAEP(sha256.New(), rand.Reader, &publicKey, payload, []byte{})
+    if err != nil {
+        fmt.Println("Error from encryption: %s\n", err)
+        return []byte{}
+    }
+    return cyphered
+}
+
+func (c *Crypto) Sign(payload []byte) []byte {
+    hashed := sha256.Sum256(payload)
+    signature, err := rsa.SignPSS(rand.Reader, c.PrivateKey, crypto.SHA256, hashed[:], nil)
+    if err != nil {
+        fmt.Println("Error from signing: %s\n", err)
+        return []byte{}
+    }
+    return signature
+}
+
+func (c *Crypto) Verify(payload, signature []byte, publicKey rsa.PublicKey) bool {
+    hashed := sha256.Sum256(payload)
+    err := rsa.VerifyPSS(&publicKey, crypto.SHA256, hashed[:], signature, nil)
+    if err != nil {
+        fmt.Println("Error from verification: %s\n", err)
+        return false
+    }
+    return true
+}
