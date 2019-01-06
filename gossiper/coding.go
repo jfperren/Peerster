69db5f4469c8d38bde12e226318a66f7f92a74f6
Julien Perrenoud <julien.perrenoud@jfperren.io>
2019-01-06 21:07:22 +0100

    Add EncodeBlock to encode message as multiple of certain length

--- gossiper/coding.go
+++ gossiper/coding.go
@@ -50,6 +50,35 @@ func Encode(structPtr interface{}, blockSize int) ([]byte, error) {
     return data, nil
 }
 
+// Encode a structure into a slice of bytes that contains its size and is padded with random bits
+func EncodeBlock(structPtr interface{}, blockSize int) ([]byte, error) {
+
+    objBytes, err := protobuf.Encode(structPtr)
+
+    if err != nil {
+        return []byte{}, err
+    }
+
+    objSize := len(objBytes)
+
+    if err != nil {
+        return []byte{}, err
+    }
+
+    totalSize := objSize + 8
+    paddedSize := totalSize + blockSize - (totalSize % blockSize)
+
+    // Copy buffer bytes into a byte slice
+    data := make([]byte, paddedSize)
+    _ = binary.PutUvarint(data[:8], uint64(len(objBytes)))
+    copy(data[8:totalSize], objBytes)
+
+    // Add padding at the end
+    rand.Read(data[totalSize:])
+
+    return data, nil
+}
+
 // Decode bytes into a structure
 func Decode(data []byte, structPtr interface{}) error {
 
