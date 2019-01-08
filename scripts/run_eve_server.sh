daea7aa8a31d350300053ddf8ae97140da877b9c
Moi <guillaume.vizier@polytechnique.edu>
2019-01-08 10:02:18 +0100

    Fix scripts for demo

--- scripts/run_eve_server.sh
+++ scripts/run_eve_server.sh
@@ -1,1 +1,1 @@
-./Peerster -gossipAddr=127.0.0.1:5000 -UIPort=8080 -name="Eve" -peers=127.0.0.1:5001 -server -rtimer=5 -cypher-if-possible=true 2> logs/eve.out
+./Peerster -gossipAddr=127.0.0.1:5004 -UIPort=8084 -name="Eve" -peers=127.0.0.1:5000 -server -rtimer=5 -cypher-if-possible=true 2> logs/eve.out
