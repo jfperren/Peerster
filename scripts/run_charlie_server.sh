a88a80fc2c6393cf53d92b0f8798f316ecde7d1a
Moi <guillaume.vizier@polytechnique.edu>
2019-01-08 09:58:25 +0100

    Put stderr in file

--- scripts/run_charlie_server.sh
+++ scripts/run_charlie_server.sh
@@ -1,1 +1,1 @@
-./Peerster -UIPort=8082 -gossipAddr=127.0.0.1:5002 -name=Charlie -peers=127.0.0.1:5000 -rtimer=5 -server
+./Peerster -UIPort=8082 -gossipAddr=127.0.0.1:5002 -name=Charlie -peers=127.0.0.1:5000 -rtimer=5 -server 2> logs/charlie.out
--- scripts/run_eve_server.sh
+++ scripts/run_eve_server.sh
@@ -1,1 +1,1 @@
-./Peerster -gossipAddr=127.0.0.1:5000 -UIPort=8080 -name="Eve" -peers=127.0.0.1:5001 -server -rtimer=5 -cypher-if-possible=true
+./Peerster -gossipAddr=127.0.0.1:5000 -UIPort=8080 -name="Eve" -peers=127.0.0.1:5001 -server -rtimer=5 -cypher-if-possible=true 2> logs/eve.out
